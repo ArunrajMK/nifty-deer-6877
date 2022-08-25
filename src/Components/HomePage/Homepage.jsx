236ae80e046f620bcbcafa32e0367172b014e19b
Vivek Goswami <101577915+vivekgoswami934@users.noreply.github.com>
2022-08-25 20:53:45 +0530

    homepage

---
+++ src/Components/HomePage/Homepage.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Box2 from "./Box2";
+import Box3 from "./Box3";
+import Box4 from "./Box4";
+
+const Homepage = () => {
+  return (
+    <div>
+      <Box2 />
+      <Box3 />
+      <Box4 />
+      <Box2 />
+    </div>
+  );
+};
+
+export default Homepage;
--- src/Components/Pages/MainRoutes.jsx
+++ src/Components/Pages/MainRoutes.jsx
@@ -2,14 +2,15 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Features from "../../Pages/Features";
 import Whyharvest from "../../Pages/Whyharvest";
+import Homepage from "../HomePage/Homepage";
 
 const MainRoutes = () => {
   return (
     <>
       <Routes>
-        <Route path="/" element={<h1>Home</h1>} />
-        <Route path="/whyHarvest" element={<Whyharvest/>} />
-        <Route path="/features" element={<Features/>}  />
+        <Route path="/" element={<Homepage />} />
+        <Route path="/whyHarvest" element={<Whyharvest />} />
+        <Route path="/features" element={<Features />} />
         <Route path="/customers" element={<h1>customers</h1>} />
         <Route path="/integration" element={<h1>integration</h1>} />
         <Route path="/pricing" element={<h1>pricing</h1>} />
