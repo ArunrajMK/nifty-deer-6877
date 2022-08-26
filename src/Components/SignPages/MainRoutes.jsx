8246d2c41a03a16d2794b7e4af6584bc8ff2b801
ArunrajMK <arunrajshanker6@gmail.com>
2022-08-26 23:26:36 +0530

    Expenses

---
+++ src/Components/SignPages/MainRoutes.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Expenses from './Expenses'
+import Invoices from './Invoices'
+
+import SecondNavbar from './SecondNavbar'
+import Time from './Time'
+
+
+function MainRoutes() {
+  return (
+    <>
+  
+    <Routes>
+    <Route path="/" element={ <SecondNavbar/>}/>
+      <Route path="/time" element={ <Time/>}/>
+      <Route path="/expenses" element={ <Expenses/>}/>
+      <Route path="/invoices" element={ <Invoices/>}/>
+    </Routes>
+    {/* ?? */}
+    </>
+  )
+}
+
+export default MainRoutes
